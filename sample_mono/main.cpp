c9771e8b26ad4f3c6434c5d667a773c9cb73dd0c
Aymeric Dujardin <aymeric.dujardin@stereolabs.com>
2024-04-03 12:55:13 +0200

    adding sample mono

--- sample_mono/main.cpp
+++ sample_mono/main.cpp
@@ -8,11 +8,8 @@
 int main(int argc, char *argv[]) {
 
     int camera_id_0 = 0;
-    int camera_id_1 = 1;
-    if (argc > 1) {
-        camera_id_0 = atoi(argv[1]);
-        camera_id_1 = atoi(argv[2]);
-    }
+    if (argc > 1) camera_id_0 = atoi(argv[1]);
+    
 
     int major, minor, patch;
     oc::ArgusVirtualCapture::getVersion(major, minor, patch);
@@ -31,7 +28,7 @@ int main(int argc, char *argv[]) {
 
     oc::ArgusCameraConfig config;
 
-    if(devs.size() >= 2) {
+    if(devs.size() >= 1) {
         if(devs.at(camera_id_0).badge == "zedx_imx678") {
             std::cout << "ZED One 4K detected!" << std::endl;
             config.mFPS = 15;
@@ -43,7 +40,6 @@ int main(int argc, char *argv[]) {
             config.mWidth = 1920;
             config.mHeight = 1200; 
         }
-
     } else {
         std::cerr << "No cameras detected" << std::endl;
         return -1;
@@ -52,74 +48,49 @@ int main(int argc, char *argv[]) {
     config.mDeviceId = (camera_id_0);
     config.verbose_level = 3;
 
-    oc::ArgusCameraConfig config_1;
-    config_1.mDeviceId = (camera_id_1);
-    config_1.mFPS = config.mFPS;
-    config_1.mWidth = config.mWidth;
-    config_1.mHeight = config.mHeight;
-    config_1.verbose_level = config.verbose_level;
-
-    oc::ArgusBayerCapture camera_0, camera_1;
+    oc::ArgusBayerCapture camera_0;
     oc::ARGUS_STATE state_cam0 = camera_0.openCamera(config);
-    oc::ARGUS_STATE state_cam1 = camera_1.openCamera(config_1);
-
-
     if (state_cam0 != oc::ARGUS_STATE::OK) {
         std::cerr << "Failed to open Camera, error code " << ARGUS_STATE2str(state_cam0) << std::endl;
         return -1;
     }
 
-    if (state_cam1 != oc::ARGUS_STATE::OK) {
-        std::cerr << "Failed to open Camera, error code " << ARGUS_STATE2str(state_cam1) << std::endl;
-        return -1;
-    }
-
-    cv::Mat rgb_cam0 = cv::Mat(config.mHeight, config.mWidth, CV_8UC4, 1);
-    cv::Mat rgb_cam1 = cv::Mat(config_1.mHeight, config_1.mWidth, CV_8UC4, 1);
-
-    cv::Mat rgb_d, rgb2_d;
+    cv::Mat rgb_d, rgb_cam0;
+    rgb_cam0 = cv::Mat(config.mHeight, config.mWidth, CV_8UC4, 1);
 
     std::cout << "Press 's' to save images" << std::endl;
 
     char key = ' ';
     int image_count = 0;
     while (key != 'q') {
-        if (camera_0.isNewFrame() && camera_1.isNewFrame()) {
+        if (camera_0.isNewFrame()) {
             memcpy(rgb_cam0.data, camera_0.getPixels(), camera_0.getWidth() * camera_0.getHeight() * camera_0.getNumberOfChannels());
-            memcpy(rgb_cam1.data, camera_1.getPixels(), camera_1.getWidth() * camera_1.getHeight() * camera_1.getNumberOfChannels());
-
-            // To get synchronized frames, a manual timestamp comparison is required
-            //std::cout << " TS : " << camera_0.getImageTimestampinUs() << " " << camera_1.getImageTimestampinUs() <<std::endl;
+            std::cout<<" TS : "<< camera_0.getImageTimestampinUs() <<std::endl;
 
 
             ///Only with argus ///
-            //std::cout<<" Exposure Time : "<<camera.getFrameExposureTime()<<" us"<<std::endl;
-            //std::cout<<" Analog Gain : "<<camera.getAnalogFrameGain()<<" dB"<<std::endl;
-            //std::cout<<" Digital Gain : "<<camera.getDigitalFrameGain()<<std::endl;
+            std::cout<<" Exposure Time : "<<camera_0.getFrameExposureTime()<<" us"<<std::endl;
+            std::cout<<" Analog Gain : "<<camera_0.getAnalogFrameGain()<<" dB"<<std::endl;
+            std::cout<<" Digital Gain : "<<camera_0.getDigitalFrameGain()<<std::endl;
             ///////////////////
 
             cv::resize(rgb_cam0, rgb_d, cv::Size(1280, 720));
-            cv::resize(rgb_cam1, rgb2_d, cv::Size(1280, 720));
 
-            cv::imshow("image Left", rgb_d);
-            cv::imshow("image Right", rgb2_d);
+            cv::imshow("image", rgb_d);
             key = cv::waitKey(2);
 
             if (key == 's') {
                 // saves the images
                 cv::imwrite("image_left_" + std::to_string(image_count) + ".png", rgb_cam0);
-                cv::imwrite("image_right_" + std::to_string(image_count) + ".png", rgb_cam1);
                 std::cout << "images created." << std::endl;
                 image_count++;
             }
-
         } else
             usleep(100);
 
 
     }
     camera_0.closeCamera();
-    camera_1.closeCamera();
 
     return 0;
 
